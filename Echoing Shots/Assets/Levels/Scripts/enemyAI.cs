647f6e686b9136abfa9843e814cf7841e3d1dd9b
Martin0823 <grokon59@gmail.com>
2025-10-06 15:45:49 -0700

    EnemyAI finished
    all donezone!!

--- Echoing Shots/Assets/Levels/Scripts/enemyAI.cs
+++ Echoing Shots/Assets/Levels/Scripts/enemyAI.cs
@@ -1,27 +1,75 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour, IDamage
 {
+
+
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
     [SerializeField] int hp;
 
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+
+
+
     Color originColor;
+
+    float shootTimer;
+    bool playerInRange;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         originColor = model.material.color;
+
+     // gamemanager.instance.updateGameGoal(1);//
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        shootTimer += Time.deltaTime;
+        if (playerInRange)
+        {
+            agent.SetDestination(gamemanager.instance.player.transform.position);
+            if (shootTimer > shootRate)
+            {
+                shoot();
+            }
+        }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange= false;
+        }
+    }
+    public void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootPos.position, Quaternion.identity);    
+    }
     
 
+
+    
     public void takeDamage(int amount)
     {
         hp -= amount;
