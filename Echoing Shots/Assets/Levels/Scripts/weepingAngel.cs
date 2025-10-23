5547297c9ec710168414e4b7ed9ec77c19283800
PsychicX720 <nick11@windstream.net>
2025-10-23 01:15:12 -0500

    my 15t, 16th, AND 17th reason why...
    Who knows anymore.... everything worked then it didn't. I am about to send my laptop through a window....

--- Echoing Shots/Assets/Levels/Scripts/weepingAngel.cs
+++ Echoing Shots/Assets/Levels/Scripts/weepingAngel.cs
@@ -13,7 +13,7 @@ public class weepingAngel : MonoBehaviour
 
     public Camera playerCam, jumpscareCam;
     public float AISpeed, catchDistance, jumpscareTime;
-    public string sceneAfterDeath;
+    //public string sceneAfterDeath;
 
     private void Update()
     {
@@ -45,6 +45,6 @@ public class weepingAngel : MonoBehaviour
     IEnumerator killPlayer()
     {
         yield return new WaitForSeconds(jumpscareTime);
-        SceneManager.LoadScene(sceneAfterDeath);
+        //SceneManager.LoadScene(sceneAfterDeath);
     }
 }
