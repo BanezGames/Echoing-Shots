a5577ebbcb277f4bcb91a10f64d4b5490eb085b6
KayFamm <kevpha2@gmail.com>
2025-11-08 10:25:02 -0600

    Adding cranking sounds

--- Echoing Shots/Assets/Levels/Scripts/crank.cs
+++ Echoing Shots/Assets/Levels/Scripts/crank.cs
@@ -7,6 +7,9 @@ public class crank : MonoBehaviour
     [SerializeField] int crankSpeed;
     [SerializeField] float rotMax;
 
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip crankSound;
+
     bool canSeePlayer;
 
     float rot;
@@ -23,17 +26,37 @@ public class crank : MonoBehaviour
     {
         if (Input.GetButton("Interact") && canSeePlayer && rot < rotMax)
         {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.clip = crankSound;
+                audioSource.loop = true;
+                audioSource.Play();
+            }
             rot += (crankSpeed * Time.deltaTime);
             turnCrank(rot);
             rotNorm = rot / rotMax;
             crankable.GetComponent<blockedPaths>().openBlocker(rotNorm);
+        } 
+        else
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+                audioSource.loop = false;
+            }
         }
 
-        if(rot >= rotMax)
+        if (rot >= rotMax)
         {
             canSeePlayer = false;
             this.GetComponent<BoxCollider>().enabled = false;
             gameManager.instance.hideInteraction();
+
+            if(audioSource.isPlaying)
+            {
+                audioSource.Stop();
+                audioSource.loop = false;
+            }
         }
     }
 
@@ -52,6 +75,12 @@ public class crank : MonoBehaviour
     {
         gameManager.instance.hideInteraction();
         canSeePlayer = false;
+
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+            audioSource.loop = false;
+        }
     }
 
     private void turnCrank(float rot)
