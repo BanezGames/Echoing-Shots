0c2b242098dafd0436484d0b9140809af1a5be02
Martin0823 <grokon59@gmail.com>
2025-11-08 19:49:18 -0800

    push
    laudanum added to the game

---
+++ Echoing Shots/Assets/Levels/Scripts/LaudanumPickup.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class LaudanumPickup : MonoBehaviour
+{
+    public itemStats laudanum;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            ConsumeLaudanum(other.GetComponent<playerController>());
+        }
+    }
+
+
+    void ConsumeLaudanum(playerController player)
+    {
+        if(player != null)
+        {
+            player.RetstoreSanity(10);
+            Destroy(gameObject);
+        }
+    }
+}
